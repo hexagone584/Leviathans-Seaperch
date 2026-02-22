bcc1d0ee12ec2326c1538137aae68d06f3ab1323
hexagone584 <yangjackiec16@gmail.com>
2026-02-22 18:38:58 -0500

    Update Seaperch.ino

--- Seaperch.ino
+++ Seaperch.ino
@@ -24,41 +24,92 @@ JoystickController controller(myusb); //instead of that
 
 
 //this will read from right to left (increases base(?)) instead of left to right traditionally
-String decToBinary(int n) {
-  int binaryplaces = 0;
-  String bin;
-  while (n > 0) {
-    int bit = n%2;
-    bin.append(bit);
-    n /= 2;
-    binaryplaces++;
-  }
-  for (int i = 0; i < 14-binaryplaces; i++) {
-    bin.append("0");
-  }
-  return bin;
+struct packet {
+  uint16_t Buttons; //we'll let the arduino handle the for loop. 
+  uint8_t LeftJoystickX;
+  uint8_t LeftJoystickY;
+  uint8_t RightJoystickX;
+  uint8_t RightJoystickY;
+  uint8_t Joystick;
+  uint8_t R2;
+  uint8_t L2;
+  uint8_t DPad;
+};
+
+// String decToBinary(int n) {
+//   //do I need to handle this case? I'm thinking Buttons would just be null
+//   //if (n == 0) { return "0"; }
+//   int binaryplaces = 0;
+//   String bin;
+//   while (n > 0) {
+//     int bit = n%2;
+//     bin.append(bit);
+//     n /= 2;
+//     binaryplaces++;
+//   }
+//   for (int i = 0; i < 14-binaryplaces; i++) {
+//     bin.append("0");
+//   }
+//   return bin;
+// }
+
+char* decToBinChar(int n) {
+    static char bin[13];
+    for (int i = 0; i < 13 && n>0; i++) {
+        bin[i] = 48 + n%2;
+        n /= 2;
+    }
+    return bin;
 }
 
 void setup() {
+  //Serial Communications
   Serial.begin(2000000);
-  while (!Serial) ; // wait for Teensy Serial Monitor
-  Serial.flush();
+  Serial1.begin(9600);
+  while (!Serial && !Serial1.availableForWrite()) ; // wait for Teensy Serial Monitor
+  
+  //Controller Setup
   myusb.begin();
   while (!controller.available());
   Serial.println("Controller's up!");
 
+  //Debug
+  Serial.println("Teensy up");
 }
 
+packet Inputs;
 void loop() {
-  myusb.Task();
+
+  //Still Testing
+  Serial.println("HEY!!");
+  //SEND DATA
+  Serial1.println("Hi Arduino");
+  Serial1.flush();
+  delay(1000);
+  //Serial.write((uint8_t*)&p, sizeof(p));
+
+  //Actual Inputs
+  //myusb.Task();
+  Inputs.Buttons = controller.getButtons();
+  Inputs.LeftJoystickX = controller.getAxis(0);
+  Inputs.LeftJoystickY = controller.getAxis(1);
+  Inputs.RightJoystickX = controller.getAxis(2);
+  Inputs.RightJoystickY = controller.getAxis(5);
+  Inputs.R2 = controller.getAxis(3);
+  Inputs.L2 = controller.getAxis(4);
+  Inputs.DPad = controller.getAxis(9);
+  //Serial.write((uint8_t*)&p, sizeof(p));
+
+
+
   //3 lines for debugging
-  Serial.print("Buttons: ");
-  Serial.print(decToBinary(controller.getButtons()));
-  Serial.print("\t");
+  //Serial.print("Buttons: ");
+  //Serial.print(decToBinary(controller.getButtons()));
+  //Serial.print("\t");
 
   //technical
   //Serial.printf(" Axes: A0: %u\tA1: %u\tA2: %u\tA3: %u\tA4: %u\tA5: %u\tA9: %u\n",controller.getAxis(0),controller.getAxis(1),controller.getAxis(2),controller.getAxis(3),controller.getAxis(4),controller.getAxis(5),controller.getAxis(9));
   //abstract
-  Serial.printf("XLeftStick: %03u YLeftStick: %03u XRightStick: %03u YRightStick: %03u L2: %03u R2: %03u DPad: %03u\n", controller.getAxis(0),controller.getAxis(1),controller.getAxis(2),controller.getAxis(5),controller.getAxis(3),controller.getAxis(4),controller.getAxis(9));
+  //Serial.printf("XLeftStick: %03u YLeftStick: %03u XRightStick: %03u YRightStick: %03u L2: %03u R2: %03u DPad: %03u\n", controller.getAxis(0),controller.getAxis(1),controller.getAxis(2),controller.getAxis(5),controller.getAxis(3),controller.getAxis(4),controller.getAxis(9));
   
 }
